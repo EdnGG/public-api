14eb5b5b1cf0d3a021321c0daf53e9afdc53f866
Eden <gresseden@gmail.com>
2018-11-14 22:08:29 -0500

    "Second Commit"

--- js/app.js
+++ js/app.js
@@ -1,62 +1,50 @@
-//$(document).ready( ()=> {
+$(document).ready( ()=> {
 // Hidding the modal content
 const modal = document.querySelector('.modal-container');
 modal.style.display = 'none'
 
-// const getUser = new Promise(function (allGood, notGood) {
+// making request with AJAX
+$.ajax({
+        url: 'https://randomuser.me/api/?results=12&inc=name,location,email,picture,cell,dob&nat=NZ,US',
+        dataType: 'json',
+        success: function (data) {
+            console.log(`Request con Ajax:`, data);
+        }
+});
 
-//     setTimeout(function () {
-//         notGood('Time is over');
-//     }, 3000)
-// })
+    
+// making request with FETCH    
+fetch('https://randomuser.me/api/?results=12') 
+    .then(res => res.json())
+    .then(data => { 
+        console.log(`Request con Fetch:`, data.results)
+        //$.each(data.results, function ( index , empl) {
+        
 
-// getUser()
-//     .then(function () {
-//         console.log('All Good')
-//     })
-//     .catch(function (message) {
-//         console.log(message)
-//     })
+        //})
+        mokUp(data.results[0])
+        
+})
 
-// Promise.all([
-//     getUser,
-//     getUser
-// ])
+function mokUp (data)  {
+    const employee = document.querySelector('.card')
+    const html = `
+   
+         <div class="card-img-container">
+             <img class="card-img" src="${data.picture.medium}" alt="profile picture">
+         </div>
+         <div class="card-info-container">
+             <h3 id="name" class="card-name cap">${data.name.first} , ${data.name.last}</h3>
+             <p class="card-text">${data.email}</p>
+             <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+         </div>
+               
+    `
+    employee.innerHTML = html;
+}
 
-$.ajax('https://randomuser.me/api/', {
-    method: 'GET',
-    success : function(data){
-        console.log(data)
-    }
+
+        
 
 })
 
-
-    fetch('https://randomuser.me/api/')
-        .then(function (response){
-            console.log(response)
-            return response.json()
-        })
-        .then( (user) => {
-            console.log('user', user.results[0].name.first)
-        })
-        .catch( (error)=> {
-            console.log('Error', error)
-        });
-//})
-
-(async function load() {
-    //await
-    async function getData(url) {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data;
-    }
-        
-    const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
-    const terrorList = await getData('https://yts.am/api/v2/list_movies.json?genre=terror')
-    const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
-    console.log('ActionList' , actionList)
-    console.log('TerrorList', terrorList)
-    console.log('AnimationList', animationList)
-}) ()
