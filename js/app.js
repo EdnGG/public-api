780649ef2773c70073ea93545c2f9540021d23fc
Eden <gresseden@gmail.com>
2018-11-22 22:39:27 -0500

    "testing #8"

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // making request with FETCH    
-const empleados = []
+let empleados = []
 
 fetch('https://randomuser.me/api/?results=12')
     .then(res => res.json())
-    .then(data => empleados => {
-         data.results
+    .then(data =>  {
+         empleados = data.results
         console.log(`Request con Fetch:`, empleados)
         mockup() // callback function
 
@@ -65,7 +65,8 @@ function filter(empleado) {
 }
 // Esta funcion se ejecuta cuando Fetch resuelve la promesa
 function mockup (){
-    empleados
+    
+
     let html = '';
     
         for( let i = 0; i < empleados.length; i+= 1) {
@@ -73,7 +74,7 @@ function mockup (){
            
             html += 
                 `
-                <div class="card" onClick="printModal('${encodeURI(JSON.stringify(empleados[i]))}')">
+                <div class="card" onClick="printModal('${i}')">
                 <div class="card-img-container">
                     <img class="card-img" src="${empleados[i].picture.medium}" alt="profile picture">
                 </div>
@@ -89,52 +90,52 @@ function mockup (){
 }
 
 //Funcion que imprime el Modal Container
-// function printModal(datos) {
-//     datos = JSON.parse(decodeURI(datos));
-//     console.log("Modal container works :)")
+function printModal(i) {
+    //datos = JSON.parse(decodeURI(datos));
+    console.log("Modal container works :)")
     
-//     let modalContainer = `
-//             <div class="modal-container">
-//                 <div class="modal">
-//                     <button type="button" id="modal-close-btn" class="modal-close-btn" onClick="closeModal()"><strong>X</strong></button>
-//                     <div class="modal-info-container">
-//                         <img class="modal-img" src="${datos.picture.large}" alt="profile picture">
-//                         <h3 id="name" class="modal-name cap">${datos.name.first}</h3>
-//                         <p class="modal-text">${datos.email}</p>
-//                         <p class="modal-text cap">${datos.location.city}</p>
-//                         <hr>
-//                             <p class="modal-text">${datos.phone}</p>
-//                             <p class="modal-text">${datos.location.city} , ${datos.location.state}</p>
-//                             <p class="modal-text">${datos.dob.date}</p>
-//                     </div>
-//                 </div>
+    let modalContainer = `
+            <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn" onClick="closeModal()"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${empleados[i].picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${empleados[i].name.first}</h3>
+                        <p class="modal-text">${empleados[i].email}</p>
+                        <p class="modal-text cap">${empleados[i].location.city}</p>
+                        <hr>
+                            <p class="modal-text">${empleados[i].phone}</p>
+                            <p class="modal-text">${empleados[i].location.city} , ${empleados[i]}.location.state}</p>
+                            <p class="modal-text">${empleados[i].dob.date}</p>
+                    </div>
+                </div>
 
 
-//                 <div class="modal-btn-container">                   
-//                     <button type="button" id="modal-prev" class="modal-prev btn" onClick="prevModal()">Prev</button>
-//                     <button type="button" id="modal-next" class="modal-next btn" onClick="nextModal()">Next</button>
-//                 </div>
-//             </div>`
-//     gallery.innerHTML = modalContainer;
-// }
+                <div class="modal-btn-container">                   
+                    <button type="button" id="modal-prev" class="modal-prev btn" onClick="printModal(${i-1})">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn" onClick="printModal(${i+1})">Next</button>
+                </div>
+            </div>`
+    gallery.innerHTML = modalContainer;
+}
 
-// function prevModal(datos) {
+// function prevModal(i) {
 //     console.log('function prevModal works :)')
-//     datos = JSON.parse(decodeURI(datos[i]));
+//     //datos = JSON.parse(decodeURI(datos[i]));
 
 //     let modalContainer = `
 //             <div class="modal-container">
 //                 <div class="modal">
 //                     <button type="button" id="modal-close-btn" class="modal-close-btn" onClick="closeModal()"><strong>X</strong></button>
 //                     <div class="modal-info-container">
-//                         <img class="modal-img" src="${datos.picture.large}" alt="profile picture">
-//                         <h3 id="name" class="modal-name cap">${datos.name.first}</h3>
-//                         <p class="modal-text">${datos.email}</p>
-//                         <p class="modal-text cap">${datos.location.city}</p>
+//                         <img class="modal-img" src="${empleados[i].picture.large}" alt="profile picture">
+//                         <h3 id="name" class="modal-name cap">${empleados[i].name.first}</h3>
+//                         <p class="modal-text">${empleados[i].email}</p>
+//                         <p class="modal-text cap">${empleados[i].location.city}</p>
 //                         <hr>
-//                             <p class="modal-text">${datos.phone}</p>
-//                             <p class="modal-text">${datos.location.city} , ${datos.location.state}</p>
-//                             <p class="modal-text">${datos.dob.date}</p>
+//                             <p class="modal-text">${empleados[i].phone}</p>
+//                             <p class="modal-text">${empleados[i].location.city} , ${empleados[i].location.state}</p>
+//                             <p class="modal-text">${empleados[i].dob.date}</p>
 //                     </div>
 //                 </div>
 
@@ -147,23 +148,23 @@ function mockup (){
 //     gallery.innerHTML = modalContainer;
 // }
 
-// function nextModal(datos) {
+// function nextModal(i) {
 //     console.log('function nextModal works :)')    
-//     datos = JSON.parse(decodeURI(datos[i]));
+//     //datos = JSON.parse(decodeURI(datos[i]));
 
 //     let modalContainer = `
 //             <div class="modal-container">
 //                 <div class="modal">
 //                     <button type="button" id="modal-close-btn" class="modal-close-btn" onClick="closeModal()"><strong>X</strong></button>
 //                     <div class="modal-info-container">
-//                         <img class="modal-img" src="${datos.picture.large}" alt="profile picture">
+//                         <img class="modal-img" src="${empleados[i].picture.large}" alt="profile picture">
 //                         <h3 id="name" class="modal-name cap">${datos.name.first}</h3>
-//                         <p class="modal-text">${datos.email}</p>
-//                         <p class="modal-text cap">${datos.location.city}</p>
+//                         <p class="modal-text">${empleados[i].email}</p>
+//                         <p class="modal-text cap">${empleados[i].location.city}</p>
 //                         <hr>
-//                             <p class="modal-text">${datos.phone}</p>
-//                             <p class="modal-text">${datos.location.city} , ${datos.location.state}</p>
-//                             <p class="modal-text">${datos.dob.date}</p>
+//                             <p class="modal-text">${empleados[i].phone}</p>
+//                             <p class="modal-text">${empleados[i].location.city} , ${empleados[i].location.state}</p>
+//                             <p class="modal-text">${empleados[i].dob.date}</p>
 //                     </div>
 //                 </div>
 
@@ -176,37 +177,16 @@ function mockup (){
 //     gallery.innerHTML = modalContainer;
 // }
 
-// //Funcion que cierra el Modal Container
-// function closeModal(datos) {
-//     datos = JSON.parse(decodeURI(datos));
-//     console.log('Works closeModal')
+//Funcion que cierra el Modal Container
+function closeModal(i) {
+    //datos = JSON.parse(decodeURI(datos));
+    console.log('Works closeModal')
 
-//     let modalContainerButton = document.querySelector('.modal-container')
-//     modalContainerButton.style.display = 'none';
-
-//     for (let i = 0; i < datos.length; i += 1) {
-//         console.log(datos.length)
-
-//         html +=
-//             `
-//                 <div class="card" onClick="printModal('${encodeURI(JSON.stringify(datos[i]))}')">
-//                 <div class="card-img-container">
-//                     <img class="card-img" src="${datos[i].picture.medium}" alt="profile picture">
-//                 </div>
-//                 <div class="card-info-container">
-//                     <h3 id="name" class="card-name cap">${datos[i].name.first} , ${datos[i].name.last}</h3>
-//                     <p class="card-text">${datos[i].email}</p>
-//                     <p class="card-text cap">${datos[i].location.city}, ${datos[i].location.state}</p>
-//                 </div>
-//                 </div>
-//                 `
-//         gallery.innerHTML = html;
-//     }
-
-//     // let gallery2 = document.querySelectorAll('.gallery')
-//     // gallery2.style.display = 'block';
-
-// }
+    let modalContainerButton = document.querySelector('.modal-container')
+    modalContainerButton.style.display = 'none';
+    mockup();
+    
+}
 
 
 
