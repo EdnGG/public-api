31bc0a29216a1bd36c142a711de091bb2f746cb5
Eden <gresseden@gmail.com>
2019-01-05 18:08:28 -0500

    First Commit

--- js/app.js
+++ js/app.js
@@ -9,42 +9,55 @@ $.ajax({
 
 //Creando un array donde se guardara la respuesta de Fetch, haciendo esta una variable global
 let empleados = []
+let schiebels = []
 
 // Seleccionando y guardando en una variable el contenedor donde se imprimira la galeria
 const gallery = document.querySelector('.gallery')
 
 // making request with FETCH    
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=10')
     .then(res => res.json())
     .then(data =>  {
         //La respuesta de fetch se asigna al array 'empleados'
         empleados = data.results 
-        //console.log(`Request con Fetch:`, empleados)
-        //Funcion callback que se encargara de pintar los empleados de la galeria
+        console.log(`Request con Fetch:`, empleados)
+        
         mockup(); 
+}).catch(err  =>{
+        console.log(err);
 })
     
-// Esta funcion se ejecuta cuando Fetch resuelve la promesa
+//Testing Fetch for personal project
+fetch('./actuators.json')
+    .then( res => res.json() )
+    .then( data => {
+        schiebels = data
+        console.log(schiebels)
+        mockup();
+}).catch(err => {
+        console.log(err)
+})
+
 function mockup() {
     let html = '';
-        for (let i = 0; i < empleados.length; i += 1) {
-            //console.log(empleados.length)
-//En el metodo printModal() se pasa por parametro el index de array empleados obtenido de la 
-//iteracion con el ciclo 'for' y  guardado con el nombre de la variable 'i'
+        for (let i = 0; i < schiebels.length; i += 1) {
+            //console.log(i, shiebels.length)
+            //En el metodo printModal() se pasa por parametro el index de array empleados obtenido de la 
+            //iteracion con el ciclo 'for' y  guardado con el nombre de la variable 'i'
             html +=
-            `   <div class="card" onClick="printModal('${i}')">
-                    <div class="card-img-container">
-                        <img class="card-img" src="${empleados[i].picture.medium}" alt="profile picture">
+                `   <div class="card" onClick="printModal('${i}')">
+                        <div class="card-img-container">
+                            <img class="card-img" src="../display.jpeg" alt="profile picture">
+                        </div>
+                        <div class="card-info-container">
+                            <h3 id="name" class="card-name cap">${schiebels[i].serialActuator} </h3>
+                            <p class="card-text serial-valve">${schiebels[i].serialValve}</p>
+                            <p class="card-text cap order-number">${schiebels[i].orderNumber}</p>
+                        </div>
                     </div>
-                    <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${empleados[i].name.first}  ${empleados[i].name.last}</h3>
-                        <p class="card-text">${empleados[i].email}</p>
-                        <p class="card-text cap">${empleados[i].location.city}</p>
-                    </div>
-                </div>
-            `
+                `
             gallery.innerHTML = html;
-        }   
+        }
 }
 
 // Selecionando y guardando el container donde estara el formulario de busqueda
@@ -65,48 +78,45 @@ const searchInput = document.getElementById('search-input');
 const submit = document.getElementById('search-submit');
 
 //Agregando el eventListener al boton 'submit'
-submit.addEventListener('click', ()=>{
-    //Guardando el valor que el cliente escriba en el campo searchInput 
-    let empleado = searchInput.value.toLowerCase()
+submit.addEventListener('click', () => {
+    let actuator = searchInput.value.toUpperCase()
     // Ejecutando la  funcion filter y pasando por parametro la variable 'empleado' 
-    filter (empleado)
-//})
-
-function filter(empleado) {
-    let card1 = document.querySelectorAll('.card')
+    filter(actuator)
+    function filter(actuator) {
+        let card1 = document.querySelectorAll('.card')
         for (let i = 0; i < card1.length; i++) {
-            let personName = card1[i].querySelector('.card-name').textContent;
-                if (personName.indexOf(empleado) === 0) {
-                    console.log(card1[i])
-                    card1[i].style.display = "";
-                } else {
-                    console.log(card1[i])
-                    card1[i].style.display = "none";
-                }
+            let serialActuator = card1[i].querySelector('.card-name').textContent;
+            let serialValve = card1[i].querySelector('.serial-valve').textContent;
+            let orderNumber = card1[i].querySelector('.order-number').textContent;
+            if (serialActuator.indexOf(actuator) === 0 || serialValve.indexOf(actuator) === 0 || orderNumber.indexOf(actuator) === 0) {
+                console.log(card1[i])
+                card1[i].style.display = "";
+            } else {
+                console.log(card1[i])
+                card1[i].style.display = "none";
+            }
+           
         }
-}
+    }
 })
 
+/* *********************** */
 //Funcion que imprime el Modal Container
-
 let modalC = document.createElement('div')
 function printModal(i) {
-    //console.log("Modal container works :)")
-    let birthday = empleados[i].dob.date;
-    birthday = birthday.substring(0, birthday.indexOf('T'));
     let modalContainer = `
             <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn" onClick="closeModal()"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="${empleados[i].picture.large}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${empleados[i].name.first} ${empleados[i].name.last}</h3>
-                        <p class="modal-text">${empleados[i].email}</p>
-                        <p class="modal-text cap">${empleados[i].location.state}</p>
+                        <img class="modal-img" src="../display.jpeg" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">Actuator Serial Number:</h3>
+                        <p class="modal-text">${schiebels[i].serialActuator}</p>
                         <hr>
-                            <p class="modal-text">Phone: ${empleados[i].phone}</p>
-                            <p class="modal-text">${empleados[i].location.street}, ${empleados[i].location.city}, ${empleados[i].location.state}, ${empleados[i].location.postcode}</p>
-                            <p class="modal-text">Birthday: ${birthday}</p>
+                            <p class="modal-text cap">Valve Serial Number:</p>
+                            <p class="modal-text">${schiebels[i].serialValve}</p>
+                            <p class="modal-text">Order Number:</p>
+                            <p class="modal-text">${schiebels[i].orderNumber}</p>
                     </div>
                 </div>
 
@@ -122,6 +132,8 @@ function printModal(i) {
     gallery.parentNode.insertBefore(modalC, gallery)
     modalC.style.display = 'block'
 }
+
+/* *********************** */
 function prevModal(i) {
     //console.log('function prevModal works :)')
     if (i === 0) { 
@@ -146,6 +158,3 @@ function closeModal() {
     mockup();
     modalC.style.display = 'none'
 }
-
-
-
